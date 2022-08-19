cab9330bb76377cb15fffc445b81359bcd7dbcc2
luizgcancian <luizgnr@hotmail.com>
2022-08-19 01:20:20 -0300

    adicionado bitcoin fetch

---
+++ js/modules/fetchBitcoin.js
@@ -0,0 +1,16 @@
+export default function initFetchBitcoin() {}
+const btcPriceSpan = document.querySelector(".btc-price");
+console.log(btcPriceSpan);
+function fetchBitcoin() {
+  const myF = fetch("https://blockchain.info/ticker");
+  myF
+    .then((r) => {
+      return r.json();
+    })
+    .then((btcJSON) => {
+      let btcDonation = 100 / btcJSON.BRL.last;
+      btcPriceSpan.innerText = btcDonation.toFixed(5);
+    });
+}
+
+fetchBitcoin();
